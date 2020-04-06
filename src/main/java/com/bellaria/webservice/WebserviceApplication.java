929ed6a3e977186dfdcfdda6db289876ac3492ec
marcosrb <m-rb-1@hotmail.com>
2020-04-06 12:28:15 +0200

    12th commit

--- src/main/java/com/bellaria/webservice/WebserviceApplication.java
+++ src/main/java/com/bellaria/webservice/WebserviceApplication.java
@@ -6,12 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebserviceApplication {
 
-	public static void main(String[] args) {
-		try {
-                    SpringApplication.run(WebserviceApplication.class, args);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-	}
+    public static void main(String[] args) {
+
+        SpringApplication.run(WebserviceApplication.class, args);
+
+    }
 
 }
